b9cd82ae7ac96ceb60da07421cdc9bf8db927651
kinelit <kinelit@mail.ru>
2017-03-21 01:48:13 +0400

    step1.01

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-export default () => console.log('HI!');
+const program = require('commander');
+
+function list(val) {
+  return val.split(',').map(Number);
+}
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<first_config> <second_config>')
+  .option('-f, --format [type]', 'Output format', list, ['ini', 'json', 'yaml'])
+  .parse(process.argv);
+
+
+console.log('  - %j', program.format);
+
+export default () => console.log('123456');
